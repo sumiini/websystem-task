90b541811badffb76f83ae972e0f15e2f496fff7
sumin <sumiiniii@ajou.ac.kr>
2020-11-03 02:04:34 +0900

    router,서버 요청 처리 완료

--- Lab9-201820775/myapp/app.js
+++ Lab9-201820775/myapp/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/result');
+var resultsRouter=require('./routes/result');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/result',resultsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Lab9-201820775/myapp/routes/result.js
+++ Lab9-201820775/myapp/routes/result.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.post('/',function(req,res,next){
+
+  res.render('result', { data: req.body});
 });
-/*
-* router.get('/result.pug',function(req,res,next){
-*  res.render('result', { title: 'Ajou Movie Information' });
-*});
-*/
 
 module.exports = router;
