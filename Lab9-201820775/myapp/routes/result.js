40dafefba087cabcb745447944bfd035bf8d4bcf
sumin <sumiiniii@ajou.ac.kr>
2020-11-03 03:32:35 +0900

    css적용 완료

--- Lab9-201820775/myapp/routes/result.js
+++ Lab9-201820775/myapp/routes/result.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const fs = require('fs')
+
+/*
+* fs.stat("./something",(err,stat)=>{
+*   if(err) console.error(err)
+*   else{
+*     console.log(stat.isDirectory())
+*     console.log(Stat.isFile())
+*   }
+* })
+* */
 
 router.post('/',function(req,res,next){
 
