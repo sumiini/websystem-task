2ef3670fdd484954e1b3212dc7bf88e80499058c
sumin <sumiiniii@ajou.ac.kr>
2020-11-03 17:51:15 +0900

    error 처리 완료

--- Lab9-201820775/myapp/routes/result.js
+++ Lab9-201820775/myapp/routes/result.js
@@ -1,21 +1,54 @@
 var express = require('express');
 var router = express.Router();
 
+var path = require('path');
+var images_path = path.resolve('./public/images/');
+
+
+const util = require('util')
 const fs = require('fs')
 
-/*
-* fs.stat("./something",(err,stat)=>{
-*   if(err) console.error(err)
-*   else{
-*     console.log(stat.isDirectory())
-*     console.log(Stat.isFile())
-*   }
-* })
-* */
 
 router.post('/',function(req,res,next){
 
-  res.render('result', { data: req.body});
+
+  fs.readFile("../views/index",function(err,tmp1){
+    fs.readdir(images_path,function(err,data1){
+
+      data1.forEach(function(element){
+
+        function myPromise(isbool){
+          return new Promise((resolve,reject)=>{
+
+            if(isbool===true){
+              resolve( res.render('result', { data: req.body}))
+            }
+            else{
+              reject(new Error(`ERROR: ${input_name} doesn't exist`))
+            }
+          })
+        }
+
+
+          myPromise(req.body.movie_imgurl.replace("/images/","")===element && req.body.movie_title!==""&&req.body.movie_actors!==""&&req.body.movie_genre!=="").then((result)=>{
+            res.render('result', { data: req.body})
+
+          })
+
+          myPromise(req.body.movie_imgurl.replace("/images/","")===element&&req.body.movie_title===""&&req.body.movie_actors!==""&&req.body.movie_genre!=="" ).then((result)=>{
+
+          }).catch((err)=>{
+            res.redirect('/')
+          })
+
+      });
+
+    });
+  });
 });
+
+
+
+
 
 module.exports = router;
