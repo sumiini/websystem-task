d7381de123fdaf16e546d83f6505521597e54aee
sumin <sumiiniii@ajou.ac.kr>
2020-11-06 23:04:19 +0900

    newmovie.pug 수정

--- Lab10_201820775/myapp9/routes/index.js
+++ Lab10_201820775/myapp9/routes/index.js
@@ -1,9 +1,15 @@
+const Movie = require('../models/movie');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'index.pug' });
 });
+
+
+router.get('/newmovie',function(req,res,next){
+  res.render('newmovie');
+})
 
 module.exports = router;
--- Lab10_201820775/myapp9/routes/movie.js
+++ Lab10_201820775/myapp9/routes/movie.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+router.post('/routes/movie/create',function(req,res,next){
+
+})
 
 module.exports = router;
