09594fa83eb4cc22e2a0a20c56e8615d6d771afc
sumin <sumiiniii@ajou.ac.kr>
2020-11-14 03:42:57 +0900

    edit 수정중,trending 못함

--- Lab11_201820775/myapp11/public/javascripts/main.js
+++ Lab11_201820775/myapp11/public/javascripts/main.js
@@ -1,44 +1,64 @@
 
-// function edit(id, title,year,url){
-//     let edit_title =  document.getElementById("edittitle");
-//     let edit_year = document.getElementById("edityear");
-//     let edit_url = document.getElementById("editimage");
-//
-//     let edit_id = document.getElementById("edit_id");
-//
-//     edit_id.value = id;
-//     edit_title.value = title;
-//     edit_year.value = year;
-//     edit_url.value = url;
-//
-// }
+function edit(id, title,year,url){
+    let edititle =  document.getElementById("edittitle");
+    let ediyear = document.getElementById("edityear");
+    let ediurl = document.getElementById("editimage");
 
-// function edit(){
-//     var xhr = new XMLHttpRequest();
-//     var etitle = document.getElementById("edittitle");
-//     var eyear = document.getElementById("edityear");
-//     var eurl = document.getElementById("editurl");
-//     var edit_id = document.getElementById("edit_id");
-//     var data={
-//         _id:edit_id,
-//         title:etitle.value,
-//         year:eyear.value,
-//         url : eurl.value
-//     }
-//
-//     xhr.onload = function(){
-//         alert("수정");
-//         if(xhr.status==200||xhr.status===201){
-//             let response = JSON.parse(xhr.response);
-//             let movies = response.movies;
-//
-//             console.log(response);
-//         }
-//         else{
-//             alert("수정 실패");
-//         }
-//     }
-// }
+    let edit_id = document.getElementById("edit_id");
+
+    edit_id.value = id;
+    edititle.value = title;
+    ediyear.value = year;
+    ediurl.value = url;
+
+}
+
+function ajaxEdit(){
+    var xhr = new XMLHttpRequest();
+    var etitle = document.getElementById("edittitle");
+    var eyear = document.getElementById("edityear");
+    var eurl = document.getElementById("editurl");
+    var edit_id = document.getElementById("edit_id");
+    var data={
+        _id:edit_id,
+        title:etitle.value,
+        year:eyear.value,
+        url : eurl.value
+    }
+
+    xhr.onload = function(){
+        alert("수정");
+        if(xhr.status==200||xhr.status===201){
+            let response = xhr.response;
+            let movies = response.movies;
+
+            let row = document.getElementById("movie"+movie._id);
+            row.innerHTML = "";
+            row.setAttribute("id", "movie"+movie._id);
+
+
+            let editInput = document.createElement("input");
+            editInput.setAttribute("type", "button");
+            let ntitle = etitle.value;
+            let nyear = eyear.value;
+            let nurl = eurl.value;
+
+            editInput.onclick = ()=>{ edit(movie._id, ntitle,nyear,nurl);};
+            editInput.value = "Call Edit";
+
+            row.appendChild(input);
+            row.appendChild(editInput);
+        }
+        else{
+            alert("수정 실패");
+        }
+    }
+
+    xhr.open("POST", "/update/"+ edit_id.value);
+    xhr.responseType = 'json';
+    xhr.setRequestHeader("Content-Type", 'application/json');
+    xhr.send(JSON.stringify(data));
+}
 
 // function deletedata(id){
 //     var xhr = new XMLHttpRequest();
